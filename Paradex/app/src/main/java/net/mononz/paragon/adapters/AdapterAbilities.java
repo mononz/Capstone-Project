e1a553bc86ee02c33f42a83edbccc060894f588d
Jared Hall <jared@mononz.com>
2016-05-07 11:54:42 +1000

    adding new key button presses to the abilities

--- Paradex/app/src/main/java/net/mononz/paragon/adapters/AdapterAbilities.java
+++ Paradex/app/src/main/java/net/mononz/paragon/adapters/AdapterAbilities.java
@@ -34,10 +34,10 @@ import butterknife.ButterKnife;
 public class AdapterAbilities extends CursorRecAdapter<AdapterAbilities.ShowViewHolder> {
 
     private GenericRequestBuilder<Uri, InputStream, SVG, PictureDrawable> requestBuilder;
+    private Context mContext;
 
     public AdapterAbilities(Context mContext, Cursor cursor) {
         super(cursor);
-
         requestBuilder = Glide.with(mContext)
                 .using(Glide.buildStreamModelLoader(Uri.class, mContext), InputStream.class)
                 .from(Uri.class)
@@ -48,6 +48,7 @@ public class AdapterAbilities extends CursorRecAdapter<AdapterAbilities.ShowView
                 .decoder(new SvgDecoder())
                 .animate(android.R.anim.fade_in)
                 .listener(new SvgSoftwareLayerSetter<Uri>());
+        this.mContext= mContext;
     }
 
     @Override
@@ -57,16 +58,32 @@ public class AdapterAbilities extends CursorRecAdapter<AdapterAbilities.ShowView
         int idx_type = cursor.getColumnIndex(Database.ability_type.name);
         int idx_description = cursor.getColumnIndex(Database.hero_ability.description);
         int idx_image = cursor.getColumnIndex(Database.hero_ability.image);
+        int idx_key_pc = cursor.getColumnIndex(Database.hero_ability.key_pc);
+        int idx_key_ps = cursor.getColumnIndex(Database.hero_ability.key_ps);
 
         final String image = cursor.getString(idx_image);
+        final String key_pc = cursor.getString(idx_key_pc);
+        final String key_ps = cursor.getString(idx_key_ps);
         final String name = cursor.getString(idx_name);
         final String type = cursor.getString(idx_type);
         final String description = cursor.getString(idx_description);
+
+        Glide.with(mContext)
+                .load(Uri.parse(Paradex.ASSET_PATH + key_pc))
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .crossFade()
+                .into(viewHolder.vKeyPC);
 
         requestBuilder
                 .diskCacheStrategy(DiskCacheStrategy.NONE)
                 .load(Uri.parse(Paradex.ASSET_PATH + image))
                 .into(viewHolder.vImage);
+
+        Glide.with(mContext)
+                .load(Uri.parse(Paradex.ASSET_PATH + key_ps))
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .crossFade()
+                .into(viewHolder.vKeyPS);
 
         viewHolder.vType.setText(type);
         viewHolder.vName.setText(name);
@@ -82,6 +99,8 @@ public class AdapterAbilities extends CursorRecAdapter<AdapterAbilities.ShowView
     public class ShowViewHolder extends RecyclerView.ViewHolder {
 
         View vView;
+        @Bind(R.id.key_pc) ImageView vKeyPC;
+        @Bind(R.id.key_ps) ImageView vKeyPS;
         @Bind(R.id.ability_image) ImageView vImage;
         @Bind(R.id.ability_name) TextView vName;
         @Bind(R.id.ability_type) TextView vType;
