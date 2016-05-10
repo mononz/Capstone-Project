09bafa1f681ee69530c14a36b516547b0d7ecc22
Jared Hall <jared@mononz.com>
2016-05-10 14:11:28 +1000

    Fixing randomiser to not display the same hero twice in a row

--- Paradex/app/src/main/java/net/mononz/paragon/widget/HeroWidgetProvider.java
+++ Paradex/app/src/main/java/net/mononz/paragon/widget/HeroWidgetProvider.java
@@ -18,6 +18,8 @@ import net.mononz.paragon.database.Database;
 
 public class HeroWidgetProvider extends AppWidgetProvider {
 
+    private String lastHeroID = "0";
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
@@ -27,16 +29,21 @@ public class HeroWidgetProvider extends AppWidgetProvider {
 
             // lets pull out a random row from the hero table
             Cursor cursor = context.getApplicationContext().getContentResolver().query(Database.hero.CONTENT_URI,
-                    new String[]{Database.hero.name, Database.hero.thumb, Database.hero.image, Database.type.name },
-                    null, null, "RANDOM() LIMIT 1");
+                    new String[]{Database.hero.FULL_ID, Database.hero.name, Database.hero.thumb, Database.hero.image, Database.type.name },
+                    Database.hero.FULL_ID + " IS NOT ?", new String[]{ lastHeroID }, "RANDOM() LIMIT 1");
 
-            DatabaseUtils.dumpCursor(cursor);
+            //DatabaseUtils.dumpCursor(cursor);
 
             if (cursor != null) {
                 if (cursor.getCount() > 0) {
                     if (cursor.moveToFirst()) {
+                        // store last hero id so the next random row won't be the same hero
+                        int idx_id = cursor.getColumnIndex(Database.hero.FULL_ID);
+                        lastHeroID = cursor.getString(idx_id);
+
                         int idx_name = cursor.getColumnIndex(Database.hero.name);
                         int idx_type = cursor.getColumnIndex(Database.type.name);
+                        // randomly use either the avatar thumb or teh full banner hero image
                         int idx_image = (Paradex.getRandomBoolean())
                                 ? cursor.getColumnIndex(Database.hero.thumb)
                                 : cursor.getColumnIndex(Database.hero.image);
